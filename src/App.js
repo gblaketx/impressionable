80ec8425e94946db1a49c0d73117fa1af1e63608
Gordon Blake <gblake@stanford.edu>
2019-05-28 14:07:10 -0700

    Customized landing page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './contract.svg';
 import './App.css';
 
 function App() {
@@ -8,17 +8,20 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Impressions - Communicate and Learn Remotely
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <div>
+        Icons made by <span />
+        <a href="https://www.freepik.com/" title="Freepik">Freepik</a>
+         <span /> from <span />
+        <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a>
+         is licensed by <span />
+        <a href="http://creativecommons.org/licenses/by/3.0/"
+          title="Creative Commons BY 3.0">
+          CC 3.0 BY
+        </a>
+      </div>
     </div>
   );
 }
